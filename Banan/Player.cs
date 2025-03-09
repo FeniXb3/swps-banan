dd2ee0adb230275e55cedf79116e9acf1ab2c219
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-03-09 10:52:44 +0100

    First meeting

---
+++ Banan/Player.cs
@@ -0,0 +1,32 @@
+class Player
+{
+    public string name;
+    public int x;
+    public int y;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+}
+
+
+/*
+Klasa Player
+- dane:
+    - name
+    - hp
+    - maxhp
+    - attackStrength
+    - stamina
+    - maxstamina
+    - weapon
+    - footSize
+- akcje:
+    - move
+    - attack
+    - heal
+    - dance
+    - pickUpItems
+
+*/
---
+++ Banan/Program.cs
@@ -0,0 +1,34 @@
+﻿Player hero = new Player("Snake");
+
+Console.WriteLine(hero.x);
+
+while (true)
+{
+    ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+    if (pressedKey.Key == ConsoleKey.A)
+    {
+        hero.x -= 1;
+    }
+    if (pressedKey.Key == ConsoleKey.D)
+    {
+        hero.x += 1;
+    }
+    if (pressedKey.Key == ConsoleKey.W)
+    {
+        hero.y += 1;
+    }
+    if (pressedKey.Key == ConsoleKey.S)
+    {
+        hero.y -= 1;
+    }
+
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"({hero.x}, {hero.y})");
+}
+
+Console.WriteLine("Press Space to continue...");
+ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+while (keyInfo.Key != ConsoleKey.Spacebar)
+{
+    keyInfo = Console.ReadKey(true);
+}
