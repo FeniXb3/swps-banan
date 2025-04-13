ba324b8425265d07f51813b78bcbd127bd0bc5a6
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-13 10:10:08 +0200

    Add guarding clause checking if direction key was pressed

--- Banan/Program.cs
+++ Banan/Program.cs
@@ -20,19 +20,21 @@ while (true)
 
     ConsoleKeyInfo pressedKey = Console.ReadKey(true);
 
-    if (directionsMap.ContainsKey(pressedKey.Key))
+    if (!directionsMap.ContainsKey(pressedKey.Key))
     {
-        Console.SetCursorPosition(hero.x, hero.y);
-        Console.Write(" ");
-        
-        Point direction = directionsMap[pressedKey.Key];
-
-        hero.x += direction.x;
-        hero.y += direction.y;
-
-        hero.x = Math.Clamp(hero.x, 0, Console.BufferWidth - 1);
-        hero.y = Math.Clamp(hero.y, 0, Console.BufferHeight - 1);
+        continue;
     }
+
+    Console.SetCursorPosition(hero.x, hero.y);
+    Console.Write(" ");
+
+    Point direction = directionsMap[pressedKey.Key];
+
+    hero.x += direction.x;
+    hero.y += direction.y;
+
+    hero.x = Math.Clamp(hero.x, 0, Console.BufferWidth - 1);
+    hero.y = Math.Clamp(hero.y, 0, Console.BufferHeight - 1);
 }
 
 Console.WriteLine("Press Space to continue...");
