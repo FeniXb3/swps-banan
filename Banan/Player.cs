e3c86db5518b0948fce503bc6e031f964662b745
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-06-01 10:22:17 +0200

    Make Player inherit from Character

--- Banan/Player.cs
+++ Banan/Player.cs
@@ -1,9 +1,5 @@
-class Player
+class Player : Character
 {
-    public string name;
-    public Point position;
-    public int speed = 1;
-    public string avatar;
     Dictionary<ConsoleKey, string> keyActionMap = new Dictionary<ConsoleKey, string>
     {
         { ConsoleKey.A, "moveLeft" },
@@ -13,55 +9,11 @@ class Player
         { ConsoleKey.C, "clone" }
     };
 
-    public Player(string name, string avatar)
+    public Player(string name, string avatar) : base(name, avatar)
     {
-        this.name = name;
-        this.avatar = avatar;
     }
 
-    public void Move(Point direction, string[] level)
-    {
-        Point target = position;
-
-        // target.x += direction.x * element.speed;
-        // target.y += direction.y * element.speed;
-
-        int signX = Math.Sign(direction.x);
-        for (int x = 1; x <= Math.Abs(direction.x * speed); x++)
-        {
-            int coordinateToTest = position.x + x * signX;
-            if (level[target.y][coordinateToTest] == '#')
-            {
-                break;
-            }
-
-            target.x = coordinateToTest;
-        }
-
-        int signY = Math.Sign(direction.y);
-        for (int y = 1; y <= Math.Abs(direction.y * speed); y++)
-        {
-            int coordinateToTest = position.y + y * signY;
-            if (level[coordinateToTest][target.x] == '#')
-            {
-                break;
-            }
-
-            target.y = coordinateToTest;
-        }
-
-        // HACK: We have to limit y before limiting x, because we use y to get row's length
-        target.y = Math.Clamp(target.y, 0, level.Length - 1);
-        target.x = Math.Clamp(target.x, 0, level[target.y].Length - 1);
-
-        if (level[target.y][target.x] != '#')
-        {
-            position = target;
-            speed += 1;
-        }
-    }
-
-    public string ChooseAction()
+    public override string ChooseAction()
     {
         ConsoleKeyInfo pressedKey = Console.ReadKey(true);
         string chosenAction = keyActionMap.GetValueOrDefault(pressedKey.Key, "pass");
@@ -69,24 +21,3 @@ class Player
         return chosenAction;
     }
 }
-
-
-/*
-Klasa Player
-- dane:
-    - name
-    - hp
-    - maxhp
-    - attackStrength
-    - stamina
-    - maxstamina
-    - weapon
-    - footSize
-- akcje:
-    - move
-    - attack
-    - heal
-    - dance
-    - pickUpItems
-
-*/
